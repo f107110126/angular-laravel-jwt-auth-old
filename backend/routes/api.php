8d13684c19f014eadc3a4159d9ee15e124be9c6c
Andy.Chen <f107110126@gmail.com>
2020-03-09 22:44:54 +0800

    configuration and create controller for jwt

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('auth')->middleware('auth')->group(function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
